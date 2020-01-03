a70516c0306131127a8f5310edbef2909b61cec8
pjhpeter <pjhpeter@163.com>
2020-01-04 04:12:46 +0800

    完成jwt验证支持

---
+++ heart-base/src/main/java/org/my/heart/utils/ResponseUtils.java
@@ -0,0 +1,22 @@
+package org.my.heart.utils;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.my.heart.constants.ContentType;
+import org.my.heart.entity.Result;
+
+/**
+ * 响应工具类
+ * 
+ * @author 彭嘉辉
+ *
+ */
+public class ResponseUtils {
+	
+	public static void buildResponseBody(HttpServletResponse response, Result result) throws IOException {
+		response.setContentType(ContentType.APPLICATION_JSON_UTF8);
+		response.getWriter().write(result.toJSONString());
+	}
+}
